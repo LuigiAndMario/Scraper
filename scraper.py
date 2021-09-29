7e05837f97812a0079267444b20371f33adcc3c2
Luigi Antoine Andrea Sansonetti <sluigi@student.ethz.ch>
2021-09-29 18:21:17 +0200

    added image url

--- scraper.py
+++ scraper.py
@@ -15,35 +15,38 @@ class EventSpider(scrapy.Spider):
             eventID = eventID.split('_')[-1]
             eventName = ''.join(entry.css('.event-info .title ::text').extract())
 
+            ### Getting image
+            imageURL = entry.css('.image::attr(style)').extract_first().split(' ')[1][4:-1]
+
             ### Getting date and place
             # TODO
             DATEPLACE = '.date-place '
             day = entry.css(DATEPLACE + '.right .day-time .day ::text').extract_first()
             time = entry.css(DATEPLACE + '.right .day-time .time ::text').extract_first()
-            date = entry.css(DATEPLACE + '.left .date ::text').extract_first()
+            date = entry.css(DATEPLACE + '.left .date ::text').extract_first().replace('.', '')
             month = entry.css(DATEPLACE + '.left .month ::text').extract_first()
-            monthNr = entry.css(DATEPLACE + '.left .month-number ::text').extract_first()
+            monthNr = entry.css(DATEPLACE + '.left .month-number ::text').extract_first().replace('.', '')
+            fullDate = '2021-' + monthNr + '-' + date + ' ' + time
 
             ### Getting location
             location = entry.css(DATEPLACE + '.location ::text').extract_first().replace('\n', '').replace('\t', '')
             locationID = ''
-            print('-> ' + location)
             if location == '':
                 location = entry.css(DATEPLACE + '.location a::text').extract_first().replace('\n', '').replace('\t', '')
                 locationID = entry.css(DATEPLACE + '.location a::attr(href)').extract_first().split('/')[-1]
 
-            ### Getting image
-            # TODO
 
 
             yield {
                 'eventID': eventID,
                 'eventName': eventName,
+                'imageURL': imageURL,
                 'day': day,
                 'time': time,
                 'date': date,
                 'month': month,
                 'monthNumber': monthNr,
+                'fullDate': fullDate,
                 'location': location,
                 'locationID': locationID,
             }
